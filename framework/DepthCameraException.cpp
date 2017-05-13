100e596d1e38eb5d629193c3d2398527ab6dbfec
Milan Gruner <milangruner@gmail.com>
2017-05-13 16:34:31 +0200

    Revert ":penguin: Makes the project build on Linux"
    This reverts commit 814a9276589b7e5775072b6b7384d5a9d3695071.

--- framework/DepthCameraException.cpp
+++ framework/DepthCameraException.cpp
@@ -11,7 +11,7 @@ DepthCameraException::DepthCameraException(std::string description, XnStatus sta
 DepthCameraException::~DepthCameraException()
 {}
 
-const char* DepthCameraException::what() const noexcept
+const char* DepthCameraException::what() const
 {
 	return m_message.c_str();
 }
--- framework/DepthCameraException.h
+++ framework/DepthCameraException.h
@@ -10,7 +10,7 @@ public:
 	DepthCameraException(std::string description, XnStatus status);
 	virtual ~DepthCameraException();
 
-	virtual const char* what() const noexcept;
+	virtual const char* what() const;
 
 protected:
 	std::string m_message;
